b242a8c5aaecfa9826433761d675270032117095
jeremy <projeremylegendre@gmail.com>
2023-03-29 22:15:33 +0200

    added sticky nav bar

--- js/main.js
+++ js/main.js
@@ -1,5 +1,21 @@
 $(document).ready(function () {
+  //nav slide toggle
   $(".icone").on("click", function () {
     $(".nav-list").slideToggle();
+    if ($("header").attr("class") == "sticky") {
+      $("header").removeClass("sticky");
+    } else {
+      $("header").addClass("sticky");
+    }
+  });
+  // sticky nav bar
+  $(window).scroll(function () {
+    let sc = $(this).scrollTop();
+    // console.log(sc);
+    if (sc > 100) {
+      $("header").addClass("sticky");
+    } else {
+      $("header").removeClass("sticky");
+    }
   });
 });
